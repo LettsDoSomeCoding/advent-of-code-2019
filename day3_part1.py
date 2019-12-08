74e4492544860c4c1b028a74d182b6e10bab5f78
LettsDoSomeCoding <owen.letts@gmail.com>
2019-12-08 22:33:32 +0000

    Further work on Day 3

--- day3_part1.py
+++ day3_part1.py
@@ -1,20 +1,25 @@
 import sys
+# from sympy import *
+from sympy.geometry import Point,Line
 
-wireLocation1 = [0,0]
-wireLocation2 = [0,0]
+wireLocation1 = Point(0,0)
+wireLocation2 = Point(0,0)
 directions1 = []
 directions2 = []
 
 def move_one_step(step, startingLocation):
+    finishingLocation = Point(0,0)
+    
     if step[0] == 'R':
-        startingLocation[0] = startingLocation[0] + int(step[1:])
+        finishingLocation = startingLocation.translate(int(step[1:]))
     elif step[0] == 'L':
-        startingLocation[0] = startingLocation[0] - int(step[1:])
+        finishingLocation = startingLocation.translate(-int(step[1:]))
     elif step[0] == 'U':
-        startingLocation[1] = startingLocation[1] + int(step[1:])
+        finishingLocation = startingLocation.translate(0, int(step[1:]))
     elif step[0] == 'D':
-        startingLocation[1] = startingLocation[1] - int(step[1:])
-
+        finishingLocation = startingLocation.translate(0, -int(step[1:]))
+    
+    return finishingLocation
 
 with open(sys.argv[1]) as f:
     for i,line in enumerate(f):
@@ -23,12 +28,31 @@ with open(sys.argv[1]) as f:
         elif i == 1:
             directions2 = line.split(',')
 
-for step1,step2 in zip(directions1,directions2):
-    move_one_step(step1,wireLocation1)
-    move_one_step(step2,wireLocation2)
+locations1 = []
+locations2 = []
 
-    if wireLocation1 == wireLocation2:
-        distance = abs(wireLocation1[0]) + abs(wireLocation1[1])
-        print("There is an intersection point at [" + str(wireLocation1[0]) + "," + str(wireLocation1[1]) + " which has a Manhattan distance of " + str(distance))
+for step1,step2 in zip(directions1,directions2):
+    wireLocation1 = move_one_step(step1,wireLocation1)
+    locations1.append(wireLocation1)
+    wireLocation2 = move_one_step(step2,wireLocation2)
+    locations2.append(wireLocation2)
+
+lines1 = []
+lines2 = []
+
+for location in locations1:
+    if locations1.index(location) != len(locations1) - 1:
+        line = Line(location,locations1[locations1.index(location) + 1])
+        lines1.append(line)
+
+for location in locations2:
+    if locations2.index(location) != len(locations2) - 1:
+        line = Line(location,locations2[locations2.index(location) + 1])
+        lines2.append(line)
+
+for line1 in lines1:
+    for line2 in lines2:
+        intersectionPoint = line1.intersection(line2)
+        print(str(intersectionPoint))
 
 print("finish")
