0a23cc54ae731b103fe4df531a8946716a465ac9
LettsDoSomeCoding <owen.letts@gmail.com>
2019-12-09 08:15:21 +0000

    More work on Day 3

--- day3_part1.py
+++ day3_part1.py
@@ -1,7 +1,8 @@
 import sys
 # from sympy import *
-from sympy.geometry import Point,Line
+from sympy.geometry import Point,Segment
 
+origin = Point(0,0)
 wireLocation1 = Point(0,0)
 wireLocation2 = Point(0,0)
 directions1 = []
@@ -37,22 +38,26 @@ for step1,step2 in zip(directions1,directions2):
     wireLocation2 = move_one_step(step2,wireLocation2)
     locations2.append(wireLocation2)
 
-lines1 = []
-lines2 = []
+segments1 = []
+segments2 = []
 
 for location in locations1:
     if locations1.index(location) != len(locations1) - 1:
-        line = Line(location,locations1[locations1.index(location) + 1])
-        lines1.append(line)
+        segment = Segment(location,locations1[locations1.index(location) + 1])
+        segments1.append(segment)
 
 for location in locations2:
     if locations2.index(location) != len(locations2) - 1:
-        line = Line(location,locations2[locations2.index(location) + 1])
-        lines2.append(line)
+        segment = Segment(location,locations2[locations2.index(location) + 1])
+        segments2.append(segment)
 
-for line1 in lines1:
-    for line2 in lines2:
-        intersectionPoint = line1.intersection(line2)
-        print(str(intersectionPoint))
+instersectionDistanceList = []
 
-print("finish")
+for segment1 in segments1:
+    for segment2 in segments2:
+        intersectionPoint = segment1.intersection(segment2)
+        if str(intersectionPoint) != "[]":
+            intersectionDistance = origin.taxicab_distance(intersectionPoint[0])
+            instersectionDistanceList.append(intersectionDistance)
+
+print("The shortest intersection distance is " + str(min(instersectionDistanceList)))
